15a45e384e09f1d73a1ed12f0b22353853ac8fcd
vicrdguez <vicrerdgz@outlook.com>
2020-05-16 22:53:22 +0200

    added unit tests

--- src/main/kotlin/com/blacksun/auth/service/AuthenticatorService.kt
+++ src/main/kotlin/com/blacksun/auth/service/AuthenticatorService.kt
@@ -3,6 +3,7 @@ package com.blacksun.auth.service
 import com.blacksun.auth.entity.Account
 import com.blacksun.auth.repository.IAccountRepository
 import com.blacksun.auth.utils.logger
+import com.blacksun.auth.web.dto.AccountRequest
 import io.micronaut.security.authentication.*
 import io.reactivex.Flowable
 import org.reactivestreams.Publisher
@@ -13,11 +14,10 @@ import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class AuthenticatorService : AuthenticationProvider
+class AuthenticatorService(
+    @Inject val repository: IAccountRepository
+) : AuthenticationProvider
 {
-    @Inject
-    private lateinit var repository: IAccountRepository
-
     override fun authenticate(authenticationRequest: AuthenticationRequest<*, *>?): Publisher<AuthenticationResponse>
     {
         return repository.findByUserName(authenticationRequest?.identity.toString())
@@ -25,5 +25,28 @@ class AuthenticatorService : AuthenticationProvider
             .map { acc -> UserDetails(acc.userName, Collections.emptyList()) as AuthenticationResponse }
             .map { acc -> Flowable.just(acc) }
             .orElse(Flowable.just(AuthenticationFailed()))
+    }
+
+    fun create(account: AccountRequest): Account
+    {
+
+        return repository.save(
+            Account(null, account.userName, account.email, account.password, null, null, null)
+        )
+    }
+
+    fun read(id: Long): Optional<Account>
+    {
+        return repository.findById(id)
+    }
+
+    fun update(account: Account): Account
+    {
+        return repository.update(account)
+    }
+
+    fun delete(account: Account)
+    {
+        return repository.delete(account)
     }
 }
--- src/main/kotlin/com/blacksun/auth/web/controller/AccountController.kt
+++ src/main/kotlin/com/blacksun/auth/web/controller/AccountController.kt
@@ -1,6 +1,7 @@
 package com.blacksun.auth.web.controller
 
 import com.blacksun.auth.service.AccountService
+import com.blacksun.auth.service.AuthenticatorService
 import com.blacksun.auth.utils.logger
 import com.blacksun.auth.web.dto.AccountRequest
 import com.blacksun.auth.web.dto.AccountResponse
@@ -8,14 +9,17 @@ import io.micronaut.http.annotation.Controller
 import javax.inject.Inject
 import io.micronaut.http.HttpResponse
 import io.micronaut.http.annotation.Body
+import io.micronaut.http.annotation.Get
 import io.micronaut.http.annotation.Post
 import io.micronaut.security.annotation.Secured
 import io.micronaut.security.rules.SecurityRule
+import io.reactivex.Flowable
+import java.lang.Exception
 
-
+@Secured(SecurityRule.IS_AUTHENTICATED)
 @Controller("/auth")
 class AccountController(
-    @Inject val service: AccountService
+    @Inject val service: AuthenticatorService
 )
 {
     val logger = logger()
@@ -25,9 +29,23 @@ class AccountController(
     fun createAccount(@Body account: AccountRequest): HttpResponse<AccountResponse>?
     {
         logger.info("the received request is [{}]", account)
-        val result = service.create(account)
 
-        return HttpResponse.ok(AccountResponse(result.userName))
+        try
+        {
+            val result = service.create(account)
+            return HttpResponse.created(AccountResponse(result.userName))
+        }
+        catch (e: Exception)
+        {
+            return HttpResponse.unprocessableEntity()
+        }
     }
 
+    @Get("/{id}")
+    fun getAccountName(id: Long): HttpResponse<AccountResponse>?
+    {
+        return service.read(id).map { t -> HttpResponse.ok(AccountResponse(t.userName)) }
+            .orElse(HttpResponse.noContent())
+    }
 }
+
